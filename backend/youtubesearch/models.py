edcfacf36323643920282e2fdfcea46c94c03e7d
shirin <shirin.jain@iitg.ac.in>
2021-12-18 20:42:05 +0530

    model for the youtube query data created

--- backend/youtubesearch/models.py
+++ backend/youtubesearch/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.utils.timezone import now
 
+"""
+We declare a YoutubeData model that has fields such as video title, 
+description of video, date of publishing, and thumbnail url.
+These feilds we extract from the youtube data api v3 and store 
+it in our database.
 
-
+"""
 class YoutubeData(models.Model):
     title = models.CharField(default="Title not available", max_length=250)
     description = models.CharField(default="Description not available", max_length=600,blank=True,null=True)
