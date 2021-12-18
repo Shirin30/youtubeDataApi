b81061f9568660bdb68ea9cc189945741ae7a4bf
shirin <shirin.jain@iitg.ac.in>
2021-12-18 20:46:09 +0530

    pagination incorporated is drf by setting page size of 25 result irems using rest_framework.pagination

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -95,10 +95,15 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+Here we specify pagination in django rest framework with page size of 25.
+so hence, on one page 25 result items are displayed. We also specify other dependencies 
+of DRF.
 
+"""
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 21,
+    'PAGE_SIZE': 25,
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
